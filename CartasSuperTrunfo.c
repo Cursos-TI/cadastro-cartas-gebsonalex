f66e008e30adb26dab4853f8d8c6742fc1728754
gebsonalex <gebsonalex1@gmail.com>
2025-10-30 20:09:37 -0300

    Fix syntax and variable declarations in CartasSuperTrunfo
    fiz as alteraçães necesarias, que pude faze, gostarias das correção necesarias

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,8 +4,35 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+int main(Desfio surpe trufo) {
+  
+  char ("[A01]");
+  char ("[A02]");
+  int ("3.000.000");
+  float ("15.000.000");
+  float ("50.000.000");
+  int ("75.000.000");
+
+  Alagoras = estado;
+  Código = A01;
+  Código = A02;
+  Cidade = Maragogi;
+  População = 3.000.00.000;
+  Aria em km = 15.000.000;
+  BIP = 50.000.000;
+  Números de pontos tustristicos = {50];
+
+  printf (" DESAFIO SUPER TRUFOR");
+  
+  printf ("estado Alagoas: %s\n"; Estado de alagoas);
+  printf ("código e %d\n" códido A01);
+  printe ("código e %d\n" código A02);
+  printf ("cidade e %s\n" cidade maragogi); 
+  printf ("população e %d\n 3.000.000.000")
+  printf (" bip e %d\n bip e 50.000.000");
+  printf ("numero de pomtos e %f\n" 50.000.000);
+    
+  
 
   // Área para entrada de dados
 
